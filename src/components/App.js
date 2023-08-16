2965def70e843febfeecc56e9d2a0342ff87e6a4
unknown <bhandariruchi498@gmail.com>
2023-08-16 20:14:15 +0530

    message

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React,{ useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [close,setClose]=useState(true);
+  const [show , setShow]=useState(false);
+  const handleShow = ()=>{
+    setShow(true);
+    setClose(true);
+  }
+  const handleClose= ()=>{
+    setClose(false);
+    setShow(true);
+
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <button onClick={handleShow}>Show</button>
+        {close===true && show===true && <div className="model-overlay">
+          <button className="model-close"onClick={handleClose}>Close</button>
+          <p className="model-p">This is the content of the modal</p>
+          </div>}
     </div>
   )
 }
